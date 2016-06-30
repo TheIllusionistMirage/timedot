ba5c2716fe138a418d5191c11e91b7bda877e11c
IanCarrasco <iansee.c@gmail.com>
2016-06-30 16:16:54 -0700

    FIXED MARGINS

--- components/Time.js
+++ components/Time.js
@@ -3,13 +3,19 @@ var React = require('react');
 var Time = React.createClass({
 	render(){
 		return(
+			<div>
 				<div id='info' className='row'>
 					<h1 className="col-xs-12 text-right pull-right" id="date"></h1>
 					<h1 className="col-xs-12 text-center" id="time"></h1>
 					<h1 className="col-xs-12 text-center" id="time-zone"></h1>
 					<h1 className="col-xs-12 text-center" id="time-offset"></h1>
 					<h1 className="col-xs-12 text-center" id="small-town"></h1>
+			    </div>
+			    <div id="dot-in-water">
+				    <h1 className = "col-xs-12 text-center" id="amphibious">THE DOT ISN'T AMPHIBIOUS.</h1>
+					<h1 className = "col-xs-12 text-center" id="land">HEAD FOR LAND.</h1>
 				</div>
+			</div>
 		)
 	}
 
