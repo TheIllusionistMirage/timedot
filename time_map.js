b96b76a8c490f564fc8dc2fefb246267189b89a4
IanCarrasco <iansee.c@gmail.com>
2016-06-29 23:38:21 -0700

    Fixed Animations

--- time_map.js
+++ time_map.js
@@ -50,33 +50,34 @@ function initMap() {
 		marker_lat = marker.getPosition().lat();
 		marker_lng = marker.getPosition().lng();
 		getTime(marker_lat,marker_lng);
-		TweenMax.to(document.getElementById('time'),1.5,{
+		TweenMax.to(document.getElementById('time'),0.5,{
 			opacity:0,
-			ease: SlowMo.ease.config(0.6, 2, true)
+			ease: Power4.easeIn
 
 		});
-		TweenMax.to(document.getElementById('time-zone'),1.5,{
+		TweenMax.to(document.getElementById('time-zone'),0.5,{
 			opacity:0,
-			ease: SlowMo.ease.config(0.6, 2, true)
+			ease: Power4.easeIn
 
 		});
-		TweenMax.to(document.getElementById('time-offset'),1.5,{
+		TweenMax.to(document.getElementById('time-offset'),0.5,{
 			opacity:0,
-			ease: SlowMo.ease.config(0.6, 2, true)
+			ease: Power4.easeIn
 		});
-		TweenMax.to(document.getElementById('small-town'),1.5,{
+		TweenMax.to(document.getElementById('small-town'),0.5,{
 			opacity:0,
-			ease: SlowMo.ease.config(0.6, 2, true)
+			ease: Power4.easeIn
 		});
+
 		marker_latlng = {marker_lat,marker_lng};
-
+     }
 	
 }
 markerCoord();
-google.maps.event.addListener(marker,'dragend',function(){markerCoord()});
+google.maps.event.addListener(marker,'dragend',function(){
+     markerCoord()
 
-timeRefresh();
-}
+});
 google.maps.event.addListener(marker,'drag',function(){markerCoord()});
 time.abort();
 }
