e7cc76ebbfa564063b8579635392a7c5bbd8be0e
IanCarrasco <iansee.c@gmail.com>
2016-06-30 01:42:52 -0700

    Animations

--- components/time_map.js
+++ components/time_map.js
@@ -22212,11 +22212,7 @@ function getTime(lat,lng){
 			}
 			catch(err){
 				document.getElementById('time').innerHTML = 'The Dot Isn\'t Amphibious.';
-                    document.getElementById('time').style.fontSize = '2.5rem';
-                    document.getElementById('small-town').innerHTML = 'Head for Land.';
-				document.getElementById('time-zone').style.display = 'none';
-				document.getElementById('time-offset').style.display = 'none';
-                    document.getElementById('date').style.display = 'none';
+                    document.getElementById('info').style.opacity = '0';
 				
 
 			}
